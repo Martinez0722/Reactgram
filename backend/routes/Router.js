3887a1454560d1368b5b833bdbe01df31f19bde5
Rafael Felipe <102329554+Martinez0722@users.noreply.github.com>
2023-06-16 11:32:46 -0300

    Criando o controller do usuário

--- backend/routes/Router.js
+++ backend/routes/Router.js
@@ -1,5 +1,7 @@
-const express = require("express");
+const express = require("express")
 const router = express();
+
+router.use("/api/users", require("./UserRoutes.js"));
 
 // test route
 router.get('/', (req, res) => {

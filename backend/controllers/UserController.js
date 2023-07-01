024796d92a5cda26bb0ede3bc535ada2ed368595
Rafael Felipe <102329554+Martinez0722@users.noreply.github.com>
2023-06-30 21:34:52 -0300

    Resgatando um usuário autenticado

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -14,10 +14,75 @@ const generateToken = (id) => {
 
 // Register user and sign in
 
-const register = async(req, res) => {
-   res.send("Registro")
+const register = async (req, res) => {
+   const {name, email, password} = req.body
+//    Check if user exists
+    const user = await User.findOne({email})
+
+    if(user) {
+        res.status(422).json({errors:["Por favor utilize outro e-mail."]})
+        return
+    }
+
+    // Generate password hash
+    const salt = await bcrypt.genSalt()
+    const passwordHash = await bcrypt.hash(password, salt)
+
+    // Create user
+    const newUser = await User.create({
+        name,
+        email,
+        password: passwordHash
+    })
+
+    // if user was successfully created
+    if(!newUser) {
+        res
+            .status(422)
+            .json({errors: ["Houve um erro, por favor tente mais tarde"]})
+            return;
+    }
+
+    res.status(201).json({
+        id: newUser._id,
+        token: generateToken(newUser._id)
+    });
 };
+
+// Sign user in
+const login = async (req, res) => {
+    const {email, password} = req.body;
+
+    const user = await User.findOne({ email })
+
+    if(!user) {
+        res.status(404).json({errors: ["Usuário não encontrado."]})
+        return
+    }
+
+    // Check if password matches
+    if(!(await bcrypt.compare(password, user.password))) {
+        res.status(422).json({errors: ["Senha inválida"]})
+        return
+    }
+
+    // Return user with token
+    res.status(201).json({
+        _id: user._id,
+        profileImage: user.profileImage,
+        token: generateToken(user._id)
+    });
+}
+
+// Get current logged in user
+const getCurrentUser = async (req, res) => {
+    const user = req.user;
+
+    res.status(200).json(user);
+}
 
 module.exports = {
     register,
+    login,
+    getCurrentUser,
 }
--- backend/middlewares/userValidations.js
+++ backend/middlewares/userValidations.js
@@ -14,9 +14,36 @@ const userCreateValidation = () => {
         .withMessage("Insira um e-mail válido."),
     body("password")
         .isString()
-]
+        .withMessage("A senha é obrigatória")
+        .isLength({min: 5})
+        .withMessage("A senha precisa ter no mínimo 5 caracteres"),
+    body("confirmpassword")
+        .isString()
+        .withMessage("A confirmação de senha é obrigatória")
+        .custom((value,{req}) => {
+            if(value != req.body.password) {
+                throw new Error("As senhas não são iguais.")
+            }
+            return true;
+        }),
+    ];
 };
+
+const loginValidation = () => {
+    return [
+        body("email")
+            .isString()
+            .withMessage("o e-mail é obrigatório")
+            .isEmail()
+            .withMessage("Insira um e-mail válido."),
+        body("password")
+            .isString()
+            .withMessage("A senha é obrigatória")
+
+    ]
+}
 
 module.exports = {
     userCreateValidation,
-}
+    loginValidation,
+};
